23f4e214c19b2497d05c5f31444f9926357813af
adeogu <Adeogu1234@gmail.com>
2025-09-15 01:46:35 +0300

    Create absolutevalues.java

---
+++ absolutevalues.java
@@ -0,0 +1,11 @@
+class absolute{
+  pubic static void main(string[]args){
+    Scanner scanner = new Scanner(system.in);
+    int num1 = scanner.nextInt();
+    if(num1<0){
+      system.out.println((num1* -1));
+    }else{
+      system.out.println((num1);
+  }
+    scanner.close();
+}
