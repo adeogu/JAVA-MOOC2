811e501816b14a69c1b282e2b5d2ec120d07ef94
adeogu <Adeogu1234@gmail.com>
2025-09-15 01:37:09 +0300

    Create squared.java

---
+++ squared.java
@@ -0,0 +1,12 @@
+class squared{
+  public static void main(string[]args){
+    Scanner scanner = new Scanner(system.in);
+    int squared = scanner.nextInt();
+    system.out.println(squared*square);
+    //or
+    //squaredd = squared*squared;
+    //system.out.println(squaredd);
+    scanner.close();
+  
+  }
+}
