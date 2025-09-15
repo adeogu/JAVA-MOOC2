34726cd23dda5cab90458aec546c157a285b3281
adeogu <Adeogu1234@gmail.com>
2025-09-15 08:41:24 +0300

    Create arewethereyet.java

---
+++ arewethereyet.java
@@ -0,0 +1,13 @@
+class arewethereyet{
+  public static void main(string[]args){
+    Scanner scanner = new scanner(system.in);
+    while(true){
+      system.out.println("input values");
+      int num1 = scanner.nextInt();
+      if(num1 == 4 ){
+        break;
+      }
+    }
+    scanner.close();
+  }
+}
