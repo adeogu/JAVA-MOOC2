2d2f2eaf4ac8b6285c068af45518bea15394cff2
adeogu <Adeogu1234@gmail.com>
2025-09-18 19:06:16 +0100

    Create numofnegatives.java

---
+++ numofnegatives.java
@@ -0,0 +1,17 @@
+class numofnegatives{
+  public static void main(string[]args){
+    Scanner scanner = new Scanner(system.in);
+    int count = 0;
+    while(true){
+      int value = scanner.nextInt();
+      if(value==0){
+        break;
+      }else if (values< 0){
+        i++
+      }
+      system.out.println("number of negatives" + count);
+    }
+    scanner.close();
+    
+  }
+}
