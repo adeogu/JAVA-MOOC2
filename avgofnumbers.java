ac16347710c67e42c58797f234be750582804f12
adeogu <Adeogu1234@gmail.com>
2025-09-18 19:17:43 +0100

    Create avgofnumbers.java

---
+++ avgofnumbers.java
@@ -0,0 +1,24 @@
+class numsumofnumbers{
+  public static void main(string[]args){
+    Scanner scanner = new Scanner(system.in);
+    int count = 0;
+    int count1 = 0;
+
+    while(true){
+      int value = scanner.nextInt();
+      if(value==0){
+        break;
+      }
+      //no need for else 
+      else{
+        count1++
+        count = count+value;
+      }
+      double avg = (double)count/count1; 
+      system.out.println("average of numbers" + avg);
+
+    }
+    scanner.close();
+    
+  }
+}
