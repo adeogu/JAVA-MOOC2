6a7e05057afa6655f8f3121f26622c4c0641f8b2
adeogu <Adeogu1234@gmail.com>
2025-09-18 19:09:40 +0100

    Create numberofnumbers.java

---
+++ numberofnumbers.java
@@ -0,0 +1,19 @@
+class numberofnumbers{
+  public static void main(string[]args){
+    Scanner scanner = new Scanner(system.in);
+    int count = 0;
+    while(true){
+      int value = scanner.nextInt();
+      if(value==0){
+        break;
+      }
+      //no need for else it can just be count++
+      else{
+        count++
+      }
+      system.out.println("number of negatives" + count);
+    }
+    scanner.close();
+    
+  }
+}
