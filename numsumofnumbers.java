8494c81ac21e0953a6abfb7f836a9c72d7f16d6b
adeogu <Adeogu1234@gmail.com>
2025-09-18 19:13:54 +0100

    Create numsumofnumbers.java

---
+++ numsumofnumbers.java
@@ -0,0 +1,25 @@
+class numsumofnumbers{
+  public static void main(string[]args){
+    Scanner scanner = new Scanner(system.in);
+    int count = 0;
+    int count1 = 0;
+
+    while(true){
+      int value = scanner.nextInt();
+      if(value==0){
+        break;
+      }
+      //no need for else it can just be count+=value
+      else{
+        count1++
+        count = count+value;
+        //or count += value
+      }
+      system.out.println("number of numbers" + count1);
+      system.out.println("sum of negatives" + count);
+
+    }
+    scanner.close();
+    
+  }
+}
