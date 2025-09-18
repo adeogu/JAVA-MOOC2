337b86a4745f1686c0e4c2c695aae6896530b3d7
adeogu <Adeogu1234@gmail.com>
2025-09-18 19:12:08 +0100

    Create sumofnumbers.java

---
+++ sumofnumbers.java
@@ -0,0 +1,20 @@
+class sumofnumbers{
+  public static void main(string[]args){
+    Scanner scanner = new Scanner(system.in);
+    int count = 0;
+    while(true){
+      int value = scanner.nextInt();
+      if(value==0){
+        break;
+      }
+      //no need for else it can just be count+=value
+      else{
+        count = count+value;
+        //or count += value
+      }
+      system.out.println("number of negatives" + count);
+    }
+    scanner.close();
+    
+  }
+}
